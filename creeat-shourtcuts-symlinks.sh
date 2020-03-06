4f61ffa60603247cfa0048fadbc05b1eeebaffe3
arosloniec <arosloniec@pretius.com>
2020-03-06 09:40:31 +0100

    Correct creating symlinks

--- creeat-shourtcuts-symlinks.sh
+++ creeat-shourtcuts-symlinks.sh
@@ -1,8 +1,13 @@
-#bin/sh
-
 echo "Creating symlinks"
-sudo ln docker-stop-all-containers.sh /usr/bin/dsac
-sudo ln docker-remove-all-containers.sh /usr/bin/drac
-sudo ln docker-remove-all-images.sh /usr/bin/drai
-sudo ln docker-remove-all.sh /usr/bin/dra
-sudo ln docker-remove-one.sh /usr/bin/dro
+
+sudo rm /usr/bin/dsac
+sudo rm /usr/bin/drac
+sudo rm /usr/bin/drai
+sudo rm /usr/bin/dra
+sudo rm /usr/bin/dro
+
+sudo ln -s $PWD/docker-stop-all-containers.sh /usr/bin/dsac
+sudo ln -s $PWD/docker-remove-all-containers.sh /usr/bin/drac
+sudo ln -s $PWD/docker-remove-all-images.sh /usr/bin/drai
+sudo ln -s $PWD/docker-remove-all.sh /usr/bin/dra
+sudo ln -s $PWD/docker-remove-one.sh /usr/bin/dro
